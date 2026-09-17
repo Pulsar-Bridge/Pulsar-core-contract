2ce6d109a837dcba220e6a306f2083f5df1ac266
had3sgames <had3sgames@gmail.com>
2026-09-17 18:48:40 +0000

    test: a Failed transaction rejects register_callback (completion)

--- src/test.rs
+++ src/test.rs
@@ -604,6 +604,17 @@ fn test_terminal_states_reject_further_transitions() {
 }
 
 #[test]
+fn test_failed_transaction_rejects_callback_completion() {
+    let h = setup();
+    let id = register_default(&h);
+    h.client
+        .fail_transaction(&id, &String::from_str(&h.env, "bad deposit"));
+
+    let res = h.client.try_register_callback(&id);
+    assert_eq!(res, Err(Ok(Error::InvalidStateTransition)));
+}
+
+#[test]
 fn test_failed_transaction_rejects_refund() {
     let h = setup();
     let id = register_default(&h);
